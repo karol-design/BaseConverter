ca57fc255c6c9eb8cd8caa059e39afd3dc952512
Karol Wojslaw <karol.wojslaw@globallogic.com>
2024-11-06 20:32:29 +0100

    Add the initial structure of the CLI class

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
                         FUNCTIONS DECLARATION
 ===========================================================================*/
 
-bool testNumConverter();
+bool testBaseConverter();
 
 /*===========================================================================
                         TYPEDEFS, MACROS AND CONSTS
@@ -38,27 +38,45 @@ static const char* BASE_TO_STRING[] = {"Binary", "Octal", "Decimal", "Hexadecima
 typedef struct {
   string numStr;
   NumBase originBase;
-} NumConverterData;
+} BaseConverterData;
+
+/// @brief Struct with the initial data for the CLI initialization
+typedef struct {
+  string appName;
+  string appDescription;
+  string appHelpContent;
+  uint64_t appVersion;
+} CliData;
 
 /*===========================================================================
-                            NumConverter CLASS
+                            BaseConverter CLASS
 ===========================================================================*/
 
-class NumConverter {
+class BaseConverter {
  public:
-  /// @brief [Constructor] Verify if the number is valid and convert it to decimal
-  /// @param _data NumConverterData struct with the number and origin base
-  NumConverter(const NumConverterData data) : _data(data) {
+  /// @brief [Constructor with data struct] Verify if the number is valid and convert it to decimal
+  /// @param _data BaseConverterData struct with the number and origin base
+  BaseConverter(const BaseConverterData data) : _data(data) {
     if (!isValidNumber()) {
       cout << "Error: invalid number (non-digit character detected)!" << endl;
     }
     _numDecimal = strToDec();
-  };
+  }
+
+  /// @brief [Constructor no parameters]
+  BaseConverter() {
+    _isValid = false; // No number and base provided
+  }
 
   /// @brief Convert the number to a selected numeral system
   /// @param target_base Target base
   /// @param result Pointer to a string for storing the result of the conversion
   void toBase(NumBase target_base, string* result) {
+    if (!_isValid) {
+      cout << "Error: the current number is not valid!" << endl;
+      return;
+    }
+
     switch (target_base) {
       case (NumBase::BASE_BIN): {
         *result = toBin();
@@ -85,8 +103,8 @@ class NumConverter {
   };
 
   /// @brief Set the number to be converted and its base
-  /// @param data NumConverterData struct with the number and origin base
-  void setNumber(const NumConverterData data) {
+  /// @param data BaseConverterData struct with the number and origin base
+  void setNumber(const BaseConverterData data) {
     _data.numStr = data.numStr;
     _data.originBase = data.originBase;
     if (!isValidNumber()) {
@@ -97,23 +115,31 @@ class NumConverter {
 
   /// @brief Print the current number and its numeral system
   void printNumber(void) {
+    if (!_isValid) {
+      cout << "Error: the current number is not valid!" << endl;
+      return;
+    }
+
     cout << "Value: " << _data.numStr << ", ";
     cout << "Numeral system: " << BASE_TO_STRING[static_cast<NUMBASE_SIZE>(_data.originBase)] << endl;
   }
 
  protected:
  private:
-  NumConverterData _data;
+  bool _isValid;  // Is the current number (to be converted) valid
+  BaseConverterData _data;
   uint64_t _numDecimal;
 
   bool isValidNumber() {
+    _isValid = true;
     // Itterate through the string and validate if contains only digits
     for (const char& c : _data.numStr) {
       if (!isdigit(c)) {
-        return false;
+        _isValid = false;
+        break;
       }
     }
-    return true;
+    return _isValid;
   }
 
   uint64_t strToDec() {
@@ -145,11 +171,63 @@ class NumConverter {
 };
 
 /*===========================================================================
+                      COmmandLineInterface CLASS
+===========================================================================*/
+
+// /// @brief
+// class CommandLineInterface {
+//  public:
+//   // Constructor
+//   CommandLineInterface() {
+//     ;
+//   }
+
+//   // Usage: BaseConv 0x4f00 BIN / BaseConv -h / BaseConv
+//   void run() {
+//     processInput();  // Process the input, i.e. arguments (incl. flags) and populate adequately the CLI config struct
+//     switch (/*MODE*/) {
+//       case (/* no arguments */): {
+//         showMenu();
+//         break;
+//       }
+//       case (/* conversion */): {
+//         handleConvert();
+//         break;
+//       }
+//       case (/* help */): {
+//         handleHelp();
+//         break;
+//       }
+//       default: {
+//         handleError();
+//         break;
+//       }
+//     }
+//   }
+
+//  protected:
+//  private:
+//   void showMenu() {}
+//   void processInput(const std::string& input) {}
+//   void handleHelp() {}
+//   void handleConvert() {}
+//   void handleError() {}
+// };
+
+/*===========================================================================
                             MAIN FUNCTION
 ===========================================================================*/
 
 int main() {
-  bool result = testNumConverter();
+  // // Instantiate the core converter and pass it to the CLI instance
+  // BaseConverterData convData = {numIn, fromBase};
+  // BaseConverter conv(convData);
+  // CommandLineInterface cli(&conv);
+
+  // // Run the CLI loop
+  // cli.run();
+
+  bool result = testBaseConverter();
   return 0;
 }
 
@@ -157,9 +235,9 @@ int main() {
                             TEST FUNCTION
 ===========================================================================*/
 
-/// @brief Test NumConverter class for all starting / target numeral system on one selected value
+/// @brief Test BaseConverter class for all starting / target numeral system on one selected value
 /// @note It is assumed that the NumBase enum starts from 0 and that the difference between two consequitive members is 1
-bool testNumConverter() {
+bool testBaseConverter() {
   const string EXPECTED_OUTPUT[NUMBASE_FILEDS][NUMBASE_FILEDS] = {{"0b101", "05", "5", "0x5"}, {"0b1000001", "0101", "65", "0x41"}, {"0b1100101", "0145", "101", "0x65"}, {"0b100000001", "0401", "257", "0x101"}};
   string numOut, numIn = "101";
   NumBase fromBase, toBase;
@@ -168,8 +246,9 @@ bool testNumConverter() {
   // Itterate though all starting bases
   for (NUMBASE_SIZE fromBaseUint = 0; fromBaseUint < NUMBASE_FILEDS; fromBaseUint++) {
     fromBase = static_cast<NumBase>(fromBaseUint);
-    NumConverterData convData = {numIn, fromBase};
-    NumConverter conv(convData);
+    BaseConverterData convData = {numIn, fromBase};
+    BaseConverter conv;
+    conv.setNumber(convData);
 
     // Itterate though all target bases
     for (NUMBASE_SIZE toBaseUint = 0; toBaseUint < NUMBASE_FILEDS; toBaseUint++) {
@@ -179,13 +258,13 @@ bool testNumConverter() {
       // Verify if the output is identical to the expected value
       string expected = EXPECTED_OUTPUT[static_cast<NUMBASE_SIZE>(fromBase)][static_cast<NUMBASE_SIZE>(toBase)];
       if (numOut != expected) {
-        cout << "Test result: failed at " << testNo << " out of " << (NUMBASE_FILEDS*NUMBASE_FILEDS) << "! (";
+        cout << "Test result: failed at " << testNo << " out of " << (NUMBASE_FILEDS * NUMBASE_FILEDS) << "! (";
         cout << "Output: " << numOut << ", expected: " << expected << ")" << endl;
         return false;
       }
       testNo++;
     }
   }
-  cout << "Test result: success (" << testNo << " out of " << (NUMBASE_FILEDS*NUMBASE_FILEDS) << " passed)!" << endl;
+  cout << "Test result: success (" << testNo << " out of " << (NUMBASE_FILEDS * NUMBASE_FILEDS) << " passed)!" << endl;
   return true;
 }
