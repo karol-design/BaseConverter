8192f3bafb70d8e65587cb691d78a9c27f3ab541
Karol Wojslaw <karol.wojslaw@globallogic.com>
2024-11-19 20:36:05 +0100

    Replace magic numbers with consts

--- src/base_converter.cc
+++ src/base_converter.cc
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+const int EXIT_ON_ERROR_VAL = -1;
+
 /// @brief Base (uint8_t) mapping of NumBase enum
 static const uint8_t BASE_TO_UINT[] = {2, 8, 10, 16};
 
@@ -110,10 +112,10 @@ uint64_t BaseConverter::strToDec() {
 
   catch (std::invalid_argument const& ex) {
     std::cout << "Invalid argument (std::invalid_argument); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   } catch (std::out_of_range const& ex) {
     std::cout << "Out of range (std::out_of_range); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   }
   uint64_t dec = static_cast<uint64_t>(val);
   return dec;
@@ -125,10 +127,10 @@ std::string BaseConverter::toDec() {
     ret = std::vformat("{:d}", std::make_format_args(num_decimal_));
   } catch (std::format_error const& ex) {
     std::cout << "Format error (std::format_error); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   } catch (std::bad_alloc const& ex) {
     std::cout << "Bad allocation (std::bad_alloc); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   }
   return ret;
 }
@@ -139,10 +141,10 @@ std::string BaseConverter::toHex() {
     ret = std::vformat("{:#x}", std::make_format_args(num_decimal_));
   } catch (std::format_error const& ex) {
     std::cout << "Format error (std::format_error); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   } catch (std::bad_alloc const& ex) {
     std::cout << "Bad allocation (std::bad_alloc); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   }
   return ret;
 }
@@ -153,10 +155,10 @@ std::string BaseConverter::toOct() {
     ret = std::vformat("{:#o}", std::make_format_args(num_decimal_));
   } catch (std::format_error const& ex) {
     std::cout << "Format error (std::format_error); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   } catch (std::bad_alloc const& ex) {
     std::cout << "Bad allocation (std::bad_alloc); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   }
   return ret;
 }
@@ -167,10 +169,10 @@ std::string BaseConverter::toBin() {
     ret = std::vformat("{:#b}", std::make_format_args(num_decimal_));
   } catch (std::format_error const& ex) {
     std::cout << "Format error (std::format_error); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   } catch (std::bad_alloc const& ex) {
     std::cout << "Bad allocation (std::bad_alloc); Cause: " << ex.what() << std::endl;
-    exit(-1);
+    exit(EXIT_ON_ERROR_VAL);
   }
   return ret;
 }
